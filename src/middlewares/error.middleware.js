17d6d860a41f377fd538c11b0938b66107a2b4e5
Jhorman Orozco <jsojhor@gmail.com>
2022-04-26 23:08:16 -0500

    [ADD] first steps added

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,8 @@
+module.exports = (err, req, res, next) => { 
+    const httpStatus = err.status || 500;
+
+    return res.status(httpStatus).json({
+        status: httpStatus,
+        message: err.message || 'Internal Server Error',
+    })
+}
